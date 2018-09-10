9b682e07b49a86ad9740ea1e3d1217079a92f0ce
pana <945009587@qq.com>
2018-09-10 19:00:08 +0800

    merge
    Signed-off-by: pana <945009587@qq.com>

--- src/run_tld.cpp
+++ src/run_tld.cpp
@@ -79,10 +79,20 @@ void print_help(char** argv){
    findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
    /// Find the convex hull object for each contour
    vector<vector<Point> >hull( contours.size() );
+   // Int type hull
+   vector<vector<int> > hullsI( contours.size() );
+   // Convexity defects
+   vector<vector<Vec4i> > defects( contours.size() );
+
    double maxArea = 0;
    vector<Point> maxContour;
    for( int i = 0; i < contours.size(); i++ ) {  
-     convexHull( Mat(contours[i]), hull[i], false ); 
+      convexHull( Mat(contours[i]), hull[i], false ); 
+      // find int type hull
+      convexHull( Mat(contours[i]), hullsI[i], false ); 
+	    // get convexity defects
+	    convexityDefects(Mat(contours[i]),hullsI[i], defects[i]);
+
      double area = contourArea(contours[i]);
      if (area > maxArea){
        maxArea = area;
@@ -100,6 +110,42 @@ void print_help(char** argv){
           //  drawContours(drawing, contours, i, Scalar(255), -1);
           drawContours( drawing, hull, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
         }
+        // draw defects
+		    size_t count = contours[i].size();
+        std::cout<<"Count : "<<count<<std::endl;
+        if( count < 300 )
+            continue;
+ 
+        vector<Vec4i>::iterator d =defects[i].begin();
+        while( d!=defects[i].end() ) {
+            Vec4i& v=(*d);
+      //       if(IndexOfBiggestContour == i)
+			// {
+ 
+                int startidx=v[0]; 
+                Point ptStart( contours[i][startidx] ); // point of the contour where the defect begins
+                int endidx=v[1]; 
+                Point ptEnd( contours[i][endidx] ); // point of the contour where the defect ends
+                int faridx=v[2]; 
+                Point ptFar( contours[i][faridx] );// the farthest from the convex hull point within the defect
+                int depth = v[3] / 256; // distance between the farthest point and the convex hull
+ 
+                if(depth > 20 && depth < 80)
+                {
+                line( drawing, ptStart, ptFar, CV_RGB(0,255,0), 2 );
+                line( drawing, ptEnd, ptFar, CV_RGB(0,255,0), 2 );
+				circle( drawing, ptStart,   4, Scalar(255,0,100), 2 );
+				circle( drawing, ptEnd,   4, Scalar(255,0,100), 2 );
+                circle( drawing, ptFar,   4, Scalar(100,0,255), 2 );
+                }
+ 
+				/*printf("start(%d,%d) end(%d,%d), far(%d,%d)\n",
+					ptStart.x, ptStart.y, ptEnd.x, ptEnd.y, ptFar.x, ptFar.y);*/
+            // }
+            d++;
+        }
+
+
       }
   //  contours.clear();
   //   hierarchy.clear();
